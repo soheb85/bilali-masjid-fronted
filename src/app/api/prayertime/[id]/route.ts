4f401a8d8a8fc906bb689d338ceda4c66213202e
soheb85 <shoebkhan3145@gmail.com>
2025-03-02 19:56:06 +0530

    g

--- src/app/api/prayertime/[id]/route.ts
+++ src/app/api/prayertime/[id]/route.ts
@@ -3,23 +3,29 @@ import PrayerTime from "@/models/PrayerTime";
 import { connectDB } from "@/lib/mongoose";
 
 // Define the GET handler for this dynamic route
-export async function GET(request: Request, context : { params: { id: string } }) {
+export async function GET(
+    request: Request,
+    { params }: { params: { id: string } }
+  ) {
     await connectDB();
-    
-    
+  
+    const {id}= params; // ✅ This is the correct way to get `id`
   
     try {
-        const {params} = context;
-        const id  = params.id;
-        
       const prayerTime = await PrayerTime.findById(id);
       if (!prayerTime) {
-        return NextResponse.json({ message: "Prayer time not found" }, { status: 404 });
+        return NextResponse.json(
+          { message: "Prayer time not found" },
+          { status: 404 }
+        );
       }
   
       return NextResponse.json(prayerTime, { status: 200 });
     } catch (error) {
-      return NextResponse.json({ message: "Error fetching prayer time", error }, { status: 500 });
+      return NextResponse.json(
+        { message: "Error fetching prayer time", error },
+        { status: 500 }
+      );
     }
   }
 
